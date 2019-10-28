0bbcc28972d3729c70873bda861491692b2911ae
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-10-28 11:24:14 -0400

    Added version specific Package.swift files to ensure that swift(>=..) work properly.  Any swift(>=..) with versions 4.2 and above may not test properly with the introduction of compiler version

---
+++ Package@swift-4.2.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CustomCoders",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CustomCoders",
+            targets: ["CustomCoders"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/TheAngryDarling/SwiftBasicCodableHelpers.git", from: "1.0.0"),
+        .package(url: "https://github.com/TheAngryDarling/SwiftClassCollections.git", from: "1.0.4"),
+        .package(url: "https://github.com/TheAngryDarling/SwiftNillable.git", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CustomCoders",
+            dependencies: ["SwiftClassCollections", "Nillable", "BasicCodableHelpers"]),
+        .testTarget(
+            name: "CustomCodersTests",
+            dependencies: ["CustomCoders"]),
+    ]
+)
